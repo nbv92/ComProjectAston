ef1ca4f0f4c79ec1548dbaa4f0536de4a04e38b7
max3221 <75674871+max3221@users.noreply.github.com>
2025-11-22 20:32:36 +0500

    Add files via upload

--- autobuss.java
+++ autobuss.java
@@ -3,15 +3,21 @@ import java.util.*;
 
 public class autobuss {
     public static void main(String[] args) {
-        List<String> buss = Arrays.asList("model1", "model22", "model3", "model4444");
+        List<String> buss = new ArrayList<>(Arrays.asList(
+                "model1", "model22", "model3", "model4444"
+        ));
 
-        // сортировка по длине строки
-        Collections.sort(buss, new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                return Integer.compare(s1.length(), s2.length());
+        // ручная пузырьковая сортировка по длине строки
+        for (int i = 0; i < buss.size() - 1; i++) {
+            for (int j = 0; j < buss.size() - 1 - i; j++) {
+                if (buss.get(j).length() > buss.get(j + 1).length()) {
+                    // меняем элементы местами
+                    String tmp = buss.get(j);
+                    buss.set(j, buss.get(j + 1));
+                    buss.set(j + 1, tmp);
+                }
             }
-        });
+        }
 
         System.out.println(buss);
     }
