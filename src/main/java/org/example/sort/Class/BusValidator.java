af6c9f3aa82c9b37010216176060466f737e7245
kindit <deltaxteam78@gmail.com>
2025-11-26 03:32:37 +0500

    Added BusValidator

---
+++ src/main/java/org/example/sort/Class/BusValidator.java
@@ -0,0 +1,27 @@
+package org.example.sort.Class;
+
+public class BusValidator {
+    public static void validateNumber(Integer number) {
+        if (number == null || number < 0) {
+            throw new IllegalArgumentException("The bus number cannot be less than zero.");
+        }
+    }
+
+    public static void validateModel(String model) {
+        if (model == null || model.isBlank()) {
+            throw new IllegalArgumentException("The bus model cannot be empty.");
+        }
+    }
+
+    public static void validateMileage(Integer mileage) {
+        if (mileage == null || mileage < 0) {
+            throw new IllegalArgumentException("The bus mileage cannot be less than zero.");
+        }
+    }
+
+    public static void validateBus(Bus bus) {
+        validateNumber(bus.getNumber());
+        validateModel(bus.getModel());
+        validateMileage(bus.getMileage());
+    }
+}
