d4e49abbc5f84c21cdd90c191b06eca67fbd0670
kindit <deltaxteam78@gmail.com>
2025-11-26 08:35:38 +0500

    This algorithm should be better

--- src/main/java/org/example/sort/sort/strategy/EvenNumberSortStrategy.java
+++ src/main/java/org/example/sort/sort/strategy/EvenNumberSortStrategy.java
@@ -7,29 +7,28 @@ import org.example.sort.sort.SortStrategy;
 import java.util.Comparator;
 
 public class EvenNumberSortStrategy implements SortStrategy<Bus> {
-
-    private final Comparator<Bus> comparator;
-
-    public EvenNumberSortStrategy(Comparator<Bus> comparator) {
-        this.comparator = comparator;
-    }
-
     @Override
     public void sort(CustomList<Bus> list, Comparator<Bus> comparator) {
         int n = list.size();
-        boolean swapped;
+        int[] evenIndexes = new int[n];
+        int count = 0;
 
+        for (int i = 0; i < n; i++) {
+            if (list.get(i).getNumber() % 2 == 0) {
+                evenIndexes[count++] = i;
+            }
+        }
+
+        boolean swapped;
         do {
             swapped = false;
-            for (int i = 0; i < n - 1; i++) {
-                Bus current = list.get(i);
-                Bus next = list.get(i + 1);
+            for (int i = 0; i < count - 1; i++) {
+                Bus a = list.get(evenIndexes[i]);
+                Bus b = list.get(evenIndexes[i + 1]);
 
-                if (current.getNumber() % 2 == 0 && next.getNumber() % 2 == 0
-                        && comparator.compare(current, next) > 0) {
-
-                    list.set(i, next);
-                    list.set(i + 1, current);
+                if (comparator.compare(a, b) > 0) {
+                    list.set(evenIndexes[i], b);
+                    list.set(evenIndexes[i + 1], a);
                     swapped = true;
                 }
             }
